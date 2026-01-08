c910177f9aa2ff5d7a62da8a58829b4adb9247d6
Arisha-Akbar <arishaakbar713@gmail.com>
2026-01-08 20:47:53 +0500

    Added alt attribute to hero image for accessibility

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ function Hero (props){
 return(
     <>
     <div className={props.cName}>
-        <img className="hero-img" src={props.heroImg}/>
+        <img className="hero-img" alt="Travel destination hero image" src={props.heroImg}/>
         <div className="hero-text">
             <h1>{props.title}</h1>
             <p>{props.text}</p>
